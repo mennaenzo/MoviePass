9411976789759f99b6032d909248dd2ab5613b66
mennaenzo <63510954+mennaenzo@users.noreply.github.com>
2020-10-19 20:15:54 -0300

    Update header.php

--- Views/header.php
+++ Views/header.php
@@ -6,13 +6,6 @@
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
      <!-- Bootstrap CSS -->
-     <?php echo ROOT; ?>
-     <br>
-     <?php echo CSS_PATH;?>
-     <br>
-     <?php echo FRONT_ROOT.VIEWS_PATH ?>
-
-
      <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>bootstrap.min.css">
      <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>estilos.css">
 
