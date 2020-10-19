1f3d9c1590df4acf307c33e4bb9040a929d7ecbe
mennaenzo <63510954+mennaenzo@users.noreply.github.com>
2020-10-19 20:06:12 -0300

    Upload Project

---
+++ Views/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+     <!-- Bootstrap CSS -->
+     <?php echo ROOT; ?>
+     <br>
+     <?php echo CSS_PATH;?>
+     <br>
+     <?php echo FRONT_ROOT.VIEWS_PATH ?>
+
+
+     <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>bootstrap.min.css">
+     <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>estilos.css">
+
+     <title>Framework Example</title>
+</head>
+<body>
---
+++ Views/nav.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+     <span class="navbar-text">
+          <strong>Framework</strong>
+     </span>
+     <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>###">XXX</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>###">XXX</a>
+          </li>          
+     </ul>
+</nav>
