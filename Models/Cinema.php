2b1c29601330548aa795dfc0cd1022502050e8f0
Martin Gomez Vega <tincho3670@gmail.com>
2020-10-19 22:18:14 -0300

    Create Models (Cinema and Movie)

---
+++ Models/Cinema.php
@@ -0,0 +1,52 @@
+<?php
+    namespace Models;
+
+    class Cinema {
+        private $id;
+        private $name;
+        private $capacity;
+        private $adress;
+        private $ticket_price;
+    }
+
+    function __constructor($id, $name, $capacity, $adress){
+        $this->id = $id;
+        $this->name = $name;
+        $this->capacity = $capacity;
+        $this->adress = $adress;
+    }
+
+    public function getId(){
+		return $this->id;
+	}
+	public function setId($id){
+		$this->id = $id;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+	public function setName($name){
+		$this->name = $name;
+	}
+
+	public function getCapacity(){
+		return $this->capacity;
+	}
+	public function setCapacity($capacity){
+		$this->capacity = $capacity;
+	}
+
+	public function getAdress(){
+		return $this->adress;
+	}
+	public function setAdress($adress){
+		$this->adress = $adress;
+    }
+    
+    public function getTicketPrice(){
+		return $this->value;
+	}
+	public function setTicketPrice($ticket_price){
+		$this->ticket_price = $ticket_price;
+	}
---
+++ Models/Movie.php
@@ -0,0 +1,68 @@
+<?php
+    namespace Models;
+
+class Movie {
+    private $id;
+    private $adult;
+    private $name;
+    private $summary;
+    private $language;
+    private $genres = array();
+}
+
+function __constructor($id, $adult, $name, $summary, $language,$genres){
+    $this->id =  $id;
+    $this->adult =  $adult;
+    $this->name =  $name;
+    $this->sumarry = $summary;
+    $this->language = $language;
+    $this->genres = $genres;
+}
+
+public function getId(){
+    return $this->id;
+}
+
+public function setId($id){
+    $this->id = $id;
+}
+
+public function getAdult(){
+    return $this->adult;
+}
+
+public function setAdult($adult){
+    $this->adult = $adult;
+}
+
+public function getName(){
+    return $this->name;
+}
+
+public function setName($name){
+    $this->name = $name;
+}
+
+public function getSummary(){
+    return $this->summary;
+}
+
+public function setSummary($summary){
+    $this->summary = $summary;
+}
+
+public function getLanguage(){
+    return $this->language;
+}
+
+public function setLanguage($language){
+    $this->language = $language;
+}
+
+public function getGenres(){
+    return $this->genres;
+}
+
+public function setGenres($genres){
+    $this->genres = $genres;
+}
