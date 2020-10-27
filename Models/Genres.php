bc1118db57f60b4f64e78c23e10e86e0e9aec42b
Federico Guevara <50757219+guevarafede@users.noreply.github.com>
2020-10-26 21:51:09 -0300

    Create Generes

---
+++ Models/Genres.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Models;
+
+
+class Genres
+{
+    private $id_api;
+    private $name;
+
+    public function setName($name){
+        $this->name=$name;
+    }
+
+    public function setId_api($id){
+        $this->id_api=$id;
+    }
+
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getId_api(){
+        return $this->id_api;
+    }
+
+}
